8e61adf00c92c970cda2d92bb013a8dc81c8dfc8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-08 23:56:57 +0000

    Deploy parch-js/docs to github.com/parch-js/docs.git:master

---
+++ api.js
@@ -0,0 +1,29 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Application",
+        "Controller",
+        "JSONSerializer",
+        "Loader",
+        "Model",
+        "ModelManager",
+        "Registry",
+        "RestSerializer",
+        "Route",
+        "RouteSegment",
+        "Router",
+        "Store",
+        "containment"
+    ],
+    "modules": [
+        "parch"
+    ],
+    "allModules": [
+        {
+            "displayName": "parch",
+            "name": "parch"
+        }
+    ],
+    "elements": []
+} };
+});
